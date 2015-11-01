9d3e929dd4bbfe2ac98fc364a20ead624a26362c
Amr Alaa <n1amr1@gmail.com>
2015-11-01 18:28:43 +0200

    Fisish toy app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   # GET /users
   # GET /users.json
   def index
@@ -18,8 +17,8 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1/edit
- def edit
-    end
+  def edit
+  end
 
   # POST /users
   # POST /users.json
@@ -62,13 +61,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
